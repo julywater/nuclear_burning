fa24b8abc560dabd0d0e8352beb80d0ec639293d
julywater <tangyike455665@gmail.com>
2014-10-08 17:22:47 -0400

    add time step restriction; add tests use mass fraction as out_puts

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ OBJ = integral.o iso7.o
 %.o: %.c
 	$(CC) -c $<
 nuc.exe: $(OBJ)
-		$(CC) -o nuc $(OBJ)
+		$(CC) -o nuc.exe $(OBJ)
 clean:
 	rm -f *.o nuc.exe
--- integral.cpp
+++ integral.cpp
@@ -1,14 +1,21 @@
 #include"nuclear.h"
-void Form_Matrix(double dt,double rate[NRATE],double y[N],double A[N][N],double B[N]){
+double aion[N]={4,12,16,20,24,28,56};
+void Form_Matrix(double &dt,double rate[NRATE],double y[N],double A[N][N],double B[N]){
 	jacob(rate,y,A);
 	ydot(rate,y,B);
+	for(int i=0;i<N;i++){
+		if(y[i]>1e-4) {
+			double tres=fabs(0.08*y[i]/B[i]);
+			if(dt>tres) dt=tres;
+		}
+	}
 	for(int i=0;i<N;i++){
 		A[i][i]+=-1.0/dt;
 		B[i]*=-1;
 	}
 }	
 
-void nuclear_step(double dt,double rho,double temp,double y[N]){
+double nuclear_step(double dt,double rho,double temp,double y[N]){
 //	double dt=0.01;  //need to have a get_dt() function;to be done
 	double rate[NRATE];
 	Linear_system L;
@@ -16,25 +23,29 @@ void nuclear_step(double dt,double rho,double temp,double y[N]){
 	double B[N]={0};
 	get_rate(temp,rho,y,rate);
 	Form_Matrix(dt,rate,y,A,B);
-//	for(int i=0;i<N;i++){
-//		for(int j=0;j<N;j++)
-//			cout<<A[i][j]<<"     ";
-//		cout<<endl;
-//	}
-//	cout<<"B\n";
-//	for(int i=0;i<N;i++)
-//		cout<<B[i]<<"     ";
-//	cout<<endl;
+/*	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++)
+			cout<<A[i][j]<<"     ";
+		cout<<endl;
+	}
+	cout<<"B\n";
+	for(int i=0;i<N;i++)
+		cout<<B[i]<<"     ";
+	cout<<endl;
+*/
 	L.initial(A,B);
 	L.naivfct();
 	L.backward();
 	L.add(y);
 //	L.print(A);
 //	L.CleanMatrix();
+	return(dt);
+	
 }
 
 int main(){
     clock_t t=clock();
+/*
 	double time[25]={
 	0.000000000000E+00,\
 	1.334306476171E-06,\
@@ -62,27 +73,37 @@ int main(){
 	4.492359731302E-01,\
 	5.000000000000E-01
 	};
-	double to=time[0];
+*/
+//	double to=time[0];
 	double rho=1e7;
 	double temp=2e9;
-	double y[N]={0.0,1.0,0.0,0,0,0,0};
-	for(int i=1;i<25;i++){
-		double t0=time[i-1];
-		double t1=time[i];
-		nuclear_step(t1-t0,rho,temp,y);
-		double ynor=0;
-		cout<<t1<<"   ";
+	double y[N]={0.0,1.0/12,0.0,0,0,0,0};
+//	for(int i=1;i<25;i++){
+	double time=0;
+	double time_stop=0.5;
+	double dt=time_stop-time;
+	int count=0;
+	while(time<time_stop){
+		count++;
+//		double t0=time[i-1];
+//		double t1=time[i];
+//		double dt=t1-t0;
+		time+=nuclear_step(dt,rho,temp,y);
+		dt=time_stop-time;
+		double xnor=0;
+		cout<<time<<"   ";
+//		cout<<t1<<"   ";
 		for(int j=0;j<N;j++){
 			if(y[j]<0) y[j]=0;
-			ynor+=y[j];
-			cout<<y[j]<<"    ";
+			xnor+=y[j]*aion[j];
+			cout<<y[j]*aion[j]<<"    ";
 		}
 		for(int j=0;j<N;j++)
-			y[j]/=ynor;
+			y[j]/=xnor;
 		cout<<endl;
 	}
 	t=clock()-t;
         printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
-	
+	cout<<count<<endl;
 	return(0);
 }
