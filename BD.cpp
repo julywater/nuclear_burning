6035742cb602b4cdea2fdace4aef70a262a77ffb
Yike Tang <yt611@pd232.(none)>
2014-10-17 15:50:59 -0400

    start higher order adaptive time step solver

---
+++ BD.cpp
@@ -0,0 +1,24 @@
+//instead of backwoard Euler we want some adaptive time step algorithm.
+//High order BD semi-implicit method 
+#include<math.h>
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class BD {
+	private :
+	double delta0[N];
+	double delta1[N]; 
+	double H;
+	int m;
+	Linear_system A;
+		
+				
+	
+	public :
+	
+
+
+
+}
